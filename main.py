ff6127130c828baea07a12876542ba83d6e0cf00
Tony Chen <yang.chen2@anu.edu.au>
2024-05-14 02:46:00 +1000

    refactor: refactor Personal_KNN_recommender

--- main.py
+++ main.py
@@ -2,8 +2,13 @@
 # import pandas as pd
 
 from utils.load_data import df
-# from recommender.users import Personal_KNN_recommender
+from recommender.users import Personal_KNN_recommender
 
 
 if __name__ == '__main__':
-    pass
+    test = Personal_KNN_recommender()
+    result = test.recommend(6, 10)
+    for i in result:
+        print(i)
+
+    test.test(10)
